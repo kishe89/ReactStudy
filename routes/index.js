fe5f580a584b4d58409c4bafd0e549ab82ffc32e
Kim JI woon <kishe56@gmail.com>
2016-12-06 21:44:31 +0900

    exampl7추가

--- routes/index.js
+++ routes/index.js
@@ -20,5 +20,20 @@ router.get('/example4', function(req, res, next) {
 router.get('/example5', function(req, res, next) {
   res.render('example5');
 });
+router.get('/example6', function(req, res, next) {
+  res.render('example6');
+});
+router.get('/example7', function(req, res, next) {
+  res.render('example7');
+});
+router.get('/example8', function(req, res, next) {
+  res.render('example8');
+});
+router.get('/example9', function(req, res, next) {
+  res.render('example9');
+});
+router.get('/example10', function(req, res, next) {
+  res.render('example10');
+});
 
 module.exports = router;
